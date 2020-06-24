b8bf7bc24be980b484c3839c1e7232c771f2d890
Hunter Henrichsen <hunter.henrichsen@gmail.com>
2020-06-24 17:11:11 -0600

    :sparkles: Add scroll delete command.

--- src/main/kotlin/com/github/shatteredsuite/scrolls/data/scroll/ScrollType.kt
+++ src/main/kotlin/com/github/shatteredsuite/scrolls/data/scroll/ScrollType.kt
@@ -15,6 +15,9 @@ import java.util.*
 class ScrollType(override val id: String, val name: String, val material: Material, val bindingData: BindingData,
                  val displays: HashMap<String, BindingDisplay>, val crafting: ScrollCrafting, val cost: CostData,
                  val infinite: Boolean, val defaultCharges: Int) : ConfigurationSerializable, Identified {
+    val placeholders: Map<String, String>
+        get() = mapOf("id" to id, "name" to name, "binding" to bindingData.type, "infinite" to infinite.toString())
+
     override fun serialize(): Map<String, Any> {
         return ConfigUtil.reflectiveSerialize(this, ScrollType::class.java)
     }
--- src/main/kotlin/com/github/shatteredsuite/scrolls/data/scroll/commands/scrolls/ScrollDeleteCommand.kt
+++ src/main/kotlin/com/github/shatteredsuite/scrolls/data/scroll/commands/scrolls/ScrollDeleteCommand.kt
@@ -1,4 +1,35 @@
 package com.github.shatteredsuite.scrolls.data.scroll.commands.scrolls
 
-class ScrollDeleteCommand {
+import com.github.shatteredsuite.core.commands.LeafCommand
+import com.github.shatteredsuite.core.commands.TabCompleters
+import com.github.shatteredsuite.core.commands.predicates.ArgMinPredicate
+import com.github.shatteredsuite.core.commands.predicates.CancelResponse
+import com.github.shatteredsuite.core.commands.predicates.CommandContext
+import com.github.shatteredsuite.scrolls.ShatteredScrolls
+import com.github.shatteredsuite.scrolls.validation.ScrollTypeValidator
+import com.github.shatteredsuite.scrolls.validation.WarpValidator
+import org.bukkit.command.Command
+import org.bukkit.command.CommandSender
+
+class ScrollDeleteCommand(val instance: ShatteredScrolls, parent: ScrollCommand) : LeafCommand(instance,
+        parent, "delete", "shatteredscrolls.command.scroll.delete", "command.scroll.delete") {
+    init {
+        contextPredicates["args"] = ArgMinPredicate(CancelResponse(this.helpPath), 1)
+        addAlias("d")
+    }
+
+    override fun execute(context: CommandContext) {
+        val scroll = ScrollTypeValidator.validate(context.args[0])
+        context.contextMessages.putAll(scroll.placeholders)
+        if(scroll.id == instance.config().defaultTypeName) {
+            context.messenger.sendMessage(context.sender, "no-delete-default", context.contextMessages, true)
+        }
+        instance.scrolls().delete(scroll.id)
+        context.messenger.sendMessage(context.sender, "delete-scroll", context.contextMessages, true)
+        return
+    }
+
+    override fun onTabComplete(sender: CommandSender, command: Command, alias: String, args: Array<out String>): MutableList<String> {
+        return TabCompleters.completeFromOptions(args, 0, instance.scrolls().all.map { it.id })
+    }
 }
