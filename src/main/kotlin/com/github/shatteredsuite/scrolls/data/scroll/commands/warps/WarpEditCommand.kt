10540e5a662dc7b649919a8dec7c687e8baea537
Hunter Henrichsen <hunter.henrichsen@gmail.com>
2020-06-24 17:46:45 -0600

    :sparkles: Add warp edit tab completions.

--- src/main/kotlin/com/github/shatteredsuite/scrolls/data/scroll/commands/warps/WarpEditCommand.kt
+++ src/main/kotlin/com/github/shatteredsuite/scrolls/data/scroll/commands/warps/WarpEditCommand.kt
@@ -1,15 +1,19 @@
 package com.github.shatteredsuite.scrolls.data.scroll.commands.warps
 
-import com.github.shatteredsuite.core.commands.ArgParser
 import com.github.shatteredsuite.core.commands.LeafCommand
+import com.github.shatteredsuite.core.commands.TabCompleters
 import com.github.shatteredsuite.core.commands.predicates.ArgMinPredicate
 import com.github.shatteredsuite.core.commands.predicates.CancelResponse
 import com.github.shatteredsuite.core.commands.predicates.CommandContext
+import com.github.shatteredsuite.core.util.StringUtil
 import com.github.shatteredsuite.core.validation.ChoiceValidator
 import com.github.shatteredsuite.scrolls.ShatteredScrolls
 import com.github.shatteredsuite.scrolls.data.warp.Warp
 import com.github.shatteredsuite.scrolls.extensions.locationFromCommandArgs
 import com.github.shatteredsuite.scrolls.validation.WarpValidator
+import org.bukkit.command.Command
+import org.bukkit.command.CommandSender
+import org.bukkit.entity.Player
 
 class WarpEditCommand(val instance: ShatteredScrolls, parent: WarpCommand) : LeafCommand(instance, parent,
     "edit", "shatteredscrolls.command.warp.edit", "command.warp.edit") {
@@ -38,5 +42,21 @@ class WarpEditCommand(val instance: ShatteredScrolls, parent: WarpCommand) : Lea
         instance.warps().delete(warp.id)
         instance.warps().register(newWarp)
         ctx.messenger.sendMessage(ctx.sender, "edit-warp", ctx.contextMessages, true)
+    }
+
+    override fun onTabComplete(sender: CommandSender, command: Command, alias: String, args: Array<out String>): List<String> {
+        val fixedArgs = StringUtil.fixArgs(args)
+        if(fixedArgs.size <= 1) {
+            return TabCompleters.completeFromOptions(fixedArgs, 0, instance.warps().all.map { it.id })
+        }
+        if(fixedArgs.size <= 2) {
+            return TabCompleters.completeFromOptions(fixedArgs, 1, listOf("id", "name", "location"))
+        }
+        if(fixedArgs.size <= 3) {
+            if(fixedArgs[2] == "location" && sender is Player) {
+                return TabCompleters.completeLocationPlayer(fixedArgs, 2, sender)
+            }
+        }
+        return emptyList()
     }
 }
